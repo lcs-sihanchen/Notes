1be03fab9c3c751dc29e77f8a30027f9f9c1d4d6
Chen Sihan <schen@lcs.on.ca>
2021-01-18 14:46:24 -0500

    Distinguish computed properties and stored properties

--- Structures.playground/Contents.swift
+++ Structures.playground/Contents.swift
@@ -1,13 +1,33 @@
 import Cocoa
 
-var str = "Hello, playground"
-
 // Syntax of a structure
+
 struct Sport {
+    
     // The value that is stored, and the property it has
+    
+    // The property that does not need to run code is called stored properties
     var name: String
+    var isOlympicSport: Bool
+    
+    // The property stored inside a structure that requires running the code to get a result is called a computed property.
+    // Return different values based on isOlympicSport values
+    var olympicStatus: String {
+        if isOlympicSport {
+            return "\(name) is an Olympic sport"
+        } else {
+            return "\(name) is not an Olympic sport"
+        }
+    }
 }
 
-var tennis = Sport(name: "Tennis")
-// tennis.name is a variable that can be changed
-print(tennis.name)
+let chessBoxing = Sport(name: "Chessboxing", isOlympicSport: false)
+print(chessBoxing.olympicStatus)
+
+//var tennis = Sport(name: "Tennis")
+//
+//// tennis.name is a variable that can be changed
+//
+//print(tennis.name)
+
+
