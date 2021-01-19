233ca5a3c2711335c4702f51094889b74b09a79c
Chen Sihan <schen@lcs.on.ca>
2021-01-19 13:24:13 -0500

    Static property introduction and its functions

--- Structure Part 2.playground/Contents.swift
+++ Structure Part 2.playground/Contents.swift
@@ -59,3 +59,25 @@ var ed = Person(name: "Ed")
 ed.familyTree
 
 
+
+struct Student {
+    // Static means the property is shared within the structure, not used in one of the instances
+    // In this case only the change in structure with trigger the change in this variable
+    static var classSize = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+        // don't have to use self. for static properties
+        Student.classSize += 1
+    }
+}
+
+// Since it is a property of a structure rather than of an instance, we call it by the structure name
+// taylor.classSize would not return any result since static property does not work for instances
+
+print(Student.classSize)
+
+let ed2 = Student(name: "Ed")
+let taylor = Student(name: "Taylor")
+
