498607ab8df6b806b5094ccdd4553483d323c981
Chen Sihan <schen@lcs.on.ca>
2021-01-19 10:05:36 -0500

    use self. + a name of the property to refer to a property in the structures(instead of parameters)

--- Structure Part 2.playground/Contents.swift
+++ Structure Part 2.playground/Contents.swift
@@ -16,3 +16,18 @@ var user = User()
 print(user.username)
 
 
+
+
+struct Person {
+    
+    var name: String
+    
+    init(name: String) {
+        // "\(name)" refers to the parameter "name" of the initializer
+        print("\(name) was born!")
+        // self.name is the property "name" in the structure
+        self.name = name
+    }
+}
+
+
