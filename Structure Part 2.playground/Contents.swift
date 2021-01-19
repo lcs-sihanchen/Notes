e3a8a2891a4b0fb3f00b6efae6044ae838e928f3
Chen Sihan <schen@lcs.on.ca>
2021-01-19 09:45:25 -0500

    Switch to class Also change the name of the documents

--- Structure Part 2.playground/Contents.swift
+++ Structure Part 2.playground/Contents.swift
@@ -1,30 +1,3 @@
 import Cocoa
 
-struct Location {
-    var x: Int
-    var y: Int
-    
-}
-
-
-// September at LCS
-var home = Location(x: 7, y: 10)
-var school = home
-
-//
-home.x = 19
-home.y = 10
-
-// Where is home?
-home.x
-home.y
-
-// Where is school?
-school.x
-school.y
-
-
-
-
-
 
--- StructureAndClasses.playground/Contents.swift
+++ StructureAndClasses.playground/Contents.swift
@@ -1,3 +1,35 @@
 import Cocoa
 
+class Location {
+    var x: Int
+    var y: Int
+    
+    init(x:Int, y:Int) {
+        self.x = x
+        self.y = y
+    }
+    
+}
+
+
+// September at LCS
+var home = Location(x: 7, y: 10)
+var school = home
+
+// Christmas location
+home.x = 19
+home.y = 10
+
+// Where is home?
+home.x
+home.y
+
+// Where is school?
+school.x
+school.y
+
+
+
+
+
 
