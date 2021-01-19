8b902be7982b263bf0a31610f5e5dd56b7189e0a
Chen Sihan <schen@lcs.on.ca>
2021-01-19 09:59:50 -0500

    Use of initializers within a structure

--- Structure Part 2.playground/Contents.swift
+++ Structure Part 2.playground/Contents.swift
@@ -1,3 +1,18 @@
 import Cocoa
 
 
+struct User {
+    var username: String
+    
+    // Initializer provides a value for each property
+    init() {
+        username = "Anonymous"
+        print("Creating a new user!")
+    }
+}
+
+// Runs the initializer code since there is nothing specified
+var user = User()
+print(user.username)
+
+
