7fcb110d30f9919e1496146533b4bb4091ae9b8a
Chen Sihan <schen@lcs.on.ca>
2021-01-19 10:17:13 -0500

    use "lazy" for properties that are not necessarily used all the time

--- Structure Part 2.playground/Contents.swift
+++ Structure Part 2.playground/Contents.swift
@@ -18,16 +18,44 @@ print(user.username)
 
 
 
+//struct Person {
+//
+//    var name: String
+//
+//    init(name: String) {
+//        // "\(name)" refers to the parameter "name" of the initializer
+//        print("\(name) was born!")
+//        // self.name is the property "name" in the structure
+//        self.name = name
+//    }
+//}
+
+
+
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
 struct Person {
-    
     var name: String
+    // Create the property familyTree upon calling
+    // If we don't want it to be created(not necessary at all times), use lazy
+
     
+    //    var familyTree = FamilyTree()
+    
+    
+    // No difference except it is not stored internally
+    lazy var familyTree = FamilyTree()
+
     init(name: String) {
-        // "\(name)" refers to the parameter "name" of the initializer
-        print("\(name) was born!")
-        // self.name is the property "name" in the structure
         self.name = name
     }
 }
+
+var ed = Person(name: "Ed")
+
+ed.familyTree
 
 
